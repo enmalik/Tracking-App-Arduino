b96f1f4cdc0f56fef1692a14d2b52b4e23cf7d2a
Nahiyan Malik <accounts@nahiyan.com>
2013-02-02 16:22:45 -0500

    Porting over Arduino files.

---
+++ Arduino/Simulation_Serial_Input_Output/Simulation_Serial_Input_Output.ino
@@ -0,0 +1,24 @@
+int ledPin = 13;
+char server[] = "google.com";
+
+void setup() {
+ Serial.begin(9600); 
+}
+
+void loop() {
+while (Serial.available() == 0);
+
+int input = Serial.read() - '0';
+
+if (input == 1) {
+ Serial.println("Legit");
+ digitalWrite(ledPin, HIGH);
+ delay(500);
+ digitalWrite(ledPin, LOW);
+ Serial.print("GET /meter.php HTTP/1.0\n");
+} else {
+  Serial.println("Flop");
+}
+
+
+}
