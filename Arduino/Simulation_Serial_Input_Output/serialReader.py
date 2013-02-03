eeee61e3d905df7a01d15e5dd1ab63d65cf63956
Vithuran Balasubramaniam <vithuranb@gmail.com>
2013-02-02 21:08:10 -0500

    testing shit

--- Arduino/Simulation_Serial_Input_Output/serialReader.py
+++ Arduino/Simulation_Serial_Input_Output/serialReader.py
@@ -1,6 +1,7 @@
 import serial
 import time
 import twitter
+import re
 
 from httplib2 import Http
 from urllib import urlencode
@@ -9,7 +10,8 @@ data = dict(rfidtag="123456", location="toronto")
 
 def receiving(ser):
     global last_received
-
+    regex = re.compile("[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]_")
+    split = re.compile(".*\n")
     buffer = ''
     while True:
         keypress = raw_input("Enter key press: ")
@@ -17,7 +19,10 @@ def receiving(ser):
         time.sleep(0.1)
         read = ser.read(ser.inWaiting())
         print read;
-        if "Legit" in read:
+        if regex.match(read):
+            read2 = read.rstrip('\r\n')
+            params = read2.split("_");
+            data = dict(rfidtag=params[0], location=params[1])
             resp, content = h.request("http://localhost:3000/main", "POST", urlencode(data))
 
 arduino = serial.Serial('COM3',9600,timeout=10)
